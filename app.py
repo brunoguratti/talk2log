1bd35456463fd13152b1f0feda728c777aff8420
Bruno Guratti <123127842+brunoguratti@users.noreply.github.com>
2024-10-11 18:14:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -211,7 +211,7 @@ if log_files:
 
     st.sidebar.button("📝 Analyze the log", on_click=set_stage, args = (1,))
     if ss.stage > 0:
-        support_info = get_support_info(file_path, emb_model, qdrant_client, 0.5)
+        support_info = get_support_info(file_path, emb_model, qdrant_client, 0.4)
         # Display the selected log file
         with st.expander("Log file content", expanded=False, icon = "📄"):
             with open(file_path, "r") as f:
